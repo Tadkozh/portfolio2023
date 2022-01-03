a0c31e5faf425a61844f533807955616b1cb1ffb
Tadkozh <tad.kozh.bihan@free.fr>
2022-01-03 18:03:05 +0100

    Accueil animation JS

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ btnMenu.addEventListener('click', () => {
 })
 
 if(window.matchMedia('(max-width: 1300px)')) {
+
     allItemNav.forEach(item => {
         item.addEventListener('click', () => {
             nav.classList.toggle('menu-visible')
@@ -20,3 +21,35 @@ if(window.matchMedia('(max-width: 1300px)')) {
         })
     })
 }
+
+// Animation écriture
+
+const txtAnim = document.querySelector('.txt-animation');
+
+let typewriter = new Typewriter(txtAnim, {
+    loop: false,
+    deleteSpeed: 20
+})
+
+typewriter
+.pauseFor(1800)
+.changeDelay(20)
+.typeString('Je suis Franck Cario')
+.pauseFor(300)
+.typeString(', <strong>Développeur Full-Stack</strong> !')
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color: #27ae60;">HTML / CSS</span> !')
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color: #ea39ff;">JavaScript</span> !')
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color: #midnightblue;">React</span> !')
+.pauseFor(1000)
+.deleteChars(7)
+.typeString('<span style="color: #ff6910;">Express</span> !')
+.pauseFor(1000)
+.deleteChars(9)
+.typeString('<span style="color: #27ae60;">Wordpress</span> !')
+.start()
