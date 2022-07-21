11b160937947a8f16b327436f5050b4b84b7e8d0
Tadkozh <tad.kozh.bihan@free.fr>
2022-07-21 20:21:47 +0200

    Add a Reading progress bar

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const nav = document.querySelector('.nav-gauche');
 const allItemNav = document.querySelectorAll('.nav-menu-item');
 const ligne = document.querySelector('.cont-ligne');
 
+// Reading progress bar
+
+const scrollIndicator = document.querySelector(".scroll-indicator");
+const content = document.querySelector(".content");
+
+const observer = new IntersectionObserver(handleIntersect).observe(content)
+
+function handleIntersect(entries){
+  const el = entries[0];
+  if(el.isIntersecting){
+    window.addEventListener("scroll", indicatorAnimation)
+  }else if(!el.isIntersecting){
+    window.removeEventListener("scroll", indicatorAnimation)
+  }
+}
+
+function indicatorAnimation() {
+
+  if(window.scrollY > content.offsetTop) {
+    const percentage = ((window.scrollY - content.offsetTop) / content.scrollHeight * 100).toFixed(2);
+    console.log(percentage/100);
+    scrollIndicator.style.transform = `scaleX(${(percentage / 100)})`
+  } else {
+    scrollIndicator.style.transform = `scaleX(0)`
+  }
+}
+
 // Menu burger
 
 btnMenu.addEventListener('click', () => {
